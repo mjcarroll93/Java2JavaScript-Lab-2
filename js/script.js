2cf0684c4f4b4fd3ebff21642f5ad366116fd023
Michael Carroll <michaelcarroll@Zipcoders-MacBook-Pro-4.local>
2016-05-20 21:27:12 -0400

    added objects

--- js/script.js
+++ js/script.js
@@ -1,29 +1,85 @@
 " use strict ";
 
-function PetChat(){
-  var display = document.getElementById("display");
-  var numPets = null;
+function Pet(name) {
+    this.name = name;
+}
 
-  var askHowMany = function(){
-    return prompt("How many pets do you have?");
-  };
+Pet.prototype.speak = function() {
+    return "hello"
+}
+Pet.prototype.getName = function() {
+    return this.name;
+}
 
-  var askPetInfo = function(){
-    for(var i=0; i<numPets; i++){
-      var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
-      var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
-      display.innerHTML += textOut;
+function Dog(name) {
+    Pet.call(this, name);
+}
+
+Dog.prototype = Object.create(Pet.prototype);
+Dog.prototype.speak = function() {
+    return "Woof!"
+}
+
+function Cat(name) {
+    Pet.call(this, name);
+}
+
+Cat.prototype = Object.create(Pet.prototype);
+Cat.prototype.speak = function() {
+    return "Meow!"
+}
+
+function Fish(name) {
+    Pet.call(this, name);
+}
+
+Fish.prototype = Object.create(Pet.prototype);
+Fish.prototype.speak = function() {
+    return "Bubbles!"
+}
+
+
+function PetChat() {
+    var display = document.getElementById("display");
+    var numPets = null;
+    var petList = [];
+
+    var askHowMany = function() {
+        return prompt("How many pets do you have?");
+    };
+
+
+    var askPetInfo = function() {
+        for (var i = 0; i < numPets; i++) {
+            var type = prompt("What is pet #" + (i + 1) + "? (cat, dog, fish)");
+            var name = prompt("What is pet #" + (i + 1) + "'s name'?");
+
+            if (type.toLowerCase() == "dog") {
+                petList.push(new Dog(name));
+            } else if (type.toLowerCase() == "cat") {
+                petList.push(new Cat(name));
+            } else if (type.toLowerCase() == "fish") {
+                petList.push(new Fish(name));
+            } else {
+                petList.push(new Pet(name));
+            }
+        }
+    };
+
+    var displayPets = function() {
+        for (var i = 0; i < petList.length; i++) {
+            display.innerHTML += petList[i].getName() + " says " + petList[i].speak() + "<br>";
+        }
     }
-  };
 
-  this.init = function(){
-    while(numPets === null){
-      numPets = askHowMany();
+    this.init = function() {
+        while (numPets === null) {
+            numPets = askHowMany();
+        }
+
+        askPetInfo();
+        displayPets();
     }
-
-    askPetInfo();
-  }
 };
 
 var chat = new PetChat();
